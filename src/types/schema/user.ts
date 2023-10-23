70b51a9601a36c4972116a1c5c7ea8488a2b2155
s-fukumoto-usj <78556485+s-fukumoto-usj@users.noreply.github.com>
2023-10-23 15:56:14 +0900

    createUnionSchemaを削除かつ利用箇所をnativeEnumに修正 (#98)

--- src/types/schema/user.ts
+++ src/types/schema/user.ts
@@ -1,9 +1,7 @@
 import { z } from 'zod'
-import createUnionSchema from '@/core/utils/zodUtil'
-
-import { USER_TYPE_KEY_LIST } from '@/shared/items/userType'
 import { paginationQuerySchema } from '@/types/schema/pagination'
 import { zNullishString, zOptionalString, zRequiredString } from '@/types/schema/base/zSchemaString'
+import { UserUserType } from '@prisma/client'
 
 // ユーザ検索スキーマ
 export const UserQuerySchema = z
@@ -19,7 +17,10 @@ export const UserQuerySchema = z
 // ユーザ作成スキーマ
 export const UserCreationSchema = z.object({
   // ユーザ種別
-  userType: createUnionSchema(USER_TYPE_KEY_LIST),
+  userType: z
+    .nativeEnum(UserUserType)
+    .nullish()
+    .transform((v) => (v === undefined ? null : v)),
   // 氏名
   name: zRequiredString(64),
   // ログインユーザID
