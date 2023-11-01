b642966cfd7c82d2ebcede270613a2b5775bdc38
Shintaro-Higashi <s-higashi@upstream-j.co.jp>
2023-11-01 13:02:50 +0900

    施設関連薬局APIのフォルダ移動、店舗変更フォームのデータ取得ロジック修正

--- src/app/api/healthFacilities/[id]/pharmacies/[relatePharmacyId]/route.ts
+++ src/app/api/healthFacilities/[id]/pharmacies/[relatePharmacyId]/route.ts
@@ -3,12 +3,17 @@ import { ByIdRequest } from '@/types'
 import { fetchHealthFacilityRelatePharmacy } from '@/servers/services/healthFacilityRelatePharmacyService'
 import { performRequest } from '@/core/utils/requestUtil'
 
+type Params = ByIdRequest & {
+  // 施設関連薬局ID
+  relatePharmacyId: string
+}
+
 /**
  * 施設関連薬局詳細情報を取得するAPIです。
  */
-export async function GET(_req: NextRequest, { params: { id } }: { params: ByIdRequest }) {
+export async function GET(_req: NextRequest, { params: { id, relatePharmacyId } }: { params: Params }) {
   return await performRequest(async () => {
-    const entity = await fetchHealthFacilityRelatePharmacy(id)
+    const entity = await fetchHealthFacilityRelatePharmacy(relatePharmacyId)
     return NextResponse.json(entity)
   })
 }
--- src/components/domains/healthFacilityRelatePharmacies/healthFacilityRelatePharmacySaveForm.tsx
+++ src/components/domains/healthFacilityRelatePharmacies/healthFacilityRelatePharmacySaveForm.tsx
@@ -6,7 +6,7 @@ import {
   HealthFacilityRelatePharmacyModel,
 } from '@/types'
 import { useForm } from '@refinedev/react-hook-form'
-import { HttpError, useOne } from '@refinedev/core'
+import { HttpError, useCustom, useOne } from '@refinedev/core'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { ControlDatePicker } from '@/components/core/form/controlDatePicker'
 import { ControlAutocomplete } from '@/components/core/form/controlAutocomplete'
@@ -18,7 +18,6 @@ import { PATIENT_SORT_TYPE_LIST } from '@/shared/items/patientSortType'
 import { useEffect, useState } from 'react'
 import { TextField } from '@mui/material'
 import { Box } from '@mui/material'
-import Typography from '@mui/material/Typography'
 
 // 施設関連薬局作成、編集フォームプロパティ
 type Props = {
@@ -55,25 +54,26 @@ export const HealthFacilityRelatePharmacySaveForm = (props: Props) => {
 
   // 編集画面での初期値を設定
   const [editSetValue, setEditSetValue] = useState(false)
-  const { data: healthFacilityRelatePharmacy } = useOne<HealthFacilityRelatePharmacyModel>({
-    resource: 'healthFacilityRelatePharmacies',
-    id: healthFacilityRelatePharmacyId as any,
-    queryOptions: {
-      enabled: !!healthFacilityRelatePharmacyId,
-    },
+  const resourceUrl = healthFacilityRelatePharmacyId
+    ? `/api/healthFacilities/${postsData?.id}/pharmacies/${healthFacilityRelatePharmacyId}`
+    : ''
+  const { data: relatePharmacy } = useCustom<HealthFacilityRelatePharmacyModel, HttpError>({
+    method: 'get',
+    url: resourceUrl,
   })
   useEffect(() => {
-    if (healthFacilityRelatePharmacy?.data) {
-      setValue('pharmacyId', healthFacilityRelatePharmacy.data.pharmacyId)
-      setValue('startDate', new Date(healthFacilityRelatePharmacy.data.startDate))
-      setValue('billingType', healthFacilityRelatePharmacy.data.billingType)
-      setValue('paymentType', healthFacilityRelatePharmacy.data.paymentType)
-      setValue('accountManageId', healthFacilityRelatePharmacy.data.accountManageId)
-      setValue('patientSortType', healthFacilityRelatePharmacy.data.patientSortType)
-      setValue('note', healthFacilityRelatePharmacy?.data?.note)
+    if (relatePharmacy?.data && !editSetValue && healthFacilityRelatePharmacyId) {
+      setValue('pharmacyId', relatePharmacy.data.pharmacyId)
+      setValue('startDate', new Date(relatePharmacy.data.startDate))
+      setValue('billingType', relatePharmacy.data.billingType)
+      setValue('paymentType', relatePharmacy.data.paymentType)
+      setValue('accountManageId', relatePharmacy.data.accountManageId)
+      setValue('patientSortType', relatePharmacy.data.patientSortType)
+      setValue('note', relatePharmacy?.data?.note)
       setEditSetValue(true)
     }
-  }, [healthFacilityRelatePharmacy?.data, setValue])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [relatePharmacy?.data, setValue])
 
   // 選択した店舗により請求関連情報を変更
   const watchPharmacyId = useWatch({ control: control, name: 'pharmacyId' })
@@ -103,98 +103,105 @@ export const HealthFacilityRelatePharmacySaveForm = (props: Props) => {
 
   // 支払い種別表示制御
   const billingType = healthFacilityRelatePharmacyId
-    ? healthFacilityRelatePharmacy?.data.billingType // 編集時
+    ? relatePharmacy?.data.billingType // 編集時
     : postsData?.billingType // 作成時
   const watchBillingType = useWatch({ control: control, name: 'billingType' })
   const showPaymentType = (watchBillingType ? watchBillingType : billingType) === 'BATCH'
 
   // 振込口座の表示制御
   const paymentType = healthFacilityRelatePharmacyId
-    ? healthFacilityRelatePharmacy?.data.paymentType // 編集時
+    ? relatePharmacy?.data.paymentType // 編集時
     : postsData?.paymentType // 作成時
   const watchPaymentType = useWatch({ control: control, name: 'paymentType' })
   const showAccountManageId = (watchPaymentType ? watchPaymentType : paymentType) === 'TRANSFER'
 
+  // フォーム表示制御
+  const showForm = healthFacilityRelatePharmacyId ? !!relatePharmacy : !!postsData
+
   return (
     <Box sx={{ minWidth: 470 }}>
-      {!isPastStartDate && (
+      {showForm && (
         <>
-          <ControlDatePicker
-            required
-            label='対応開始日'
-            name='startDate'
-            control={control}
-            error={!!errors.startDate}
-            helperText={
-              '変更予約を設定します。指定日深夜に変更が適用されます。該当日を迎えるまでは予約内容の取消及び変更が可能です。 ' +
-              (errors.startDate?.message || '')
-            }
-            disablePast
-          />
-          <ControlAutocomplete
-            required
-            resource='pharmacies'
-            label='店舗名'
-            name='pharmacyId'
-            control={control}
-            error={!!errors.pharmacyId}
-            helperText={
-              '選択した新しい店舗に合わせて施設の振込口座を変更します。 ' + (errors.pharmacyId?.message || '')
-            }
-            optionLabel={(option: any) => {
-              return option?.pharmacyGroup?.name + ' ' + option?.name ?? ''
-            }}
-          />
-          <ControlItemAutocomplete
-            label='請求種別'
-            name='billingType'
-            options={BILLING_TYPE_LIST}
-            control={control}
-            error={!!errors.billingType}
-            helperText={errors.billingType?.message}
-          />
-          {showPaymentType && (
-            <ControlItemAutocomplete
-              label='支払い種別'
-              name='paymentType'
-              options={HEALTH_FACILITY_PAYMENT_TYPE_LIST}
-              control={control}
-              error={!!errors.paymentType}
-              helperText={errors.paymentType?.message}
-            />
+          {!isPastStartDate && (
+            <>
+              <ControlDatePicker
+                required
+                label='対応開始日'
+                name='startDate'
+                control={control}
+                error={!!errors.startDate}
+                helperText={
+                  '変更予約を設定します。指定日深夜に変更が適用されます。該当日を迎えるまでは予約内容の取消及び変更が可能です。 ' +
+                  (errors.startDate?.message || '')
+                }
+                disablePast
+              />
+              <ControlAutocomplete
+                required
+                resource='pharmacies'
+                label='店舗名'
+                name='pharmacyId'
+                control={control}
+                error={!!errors.pharmacyId}
+                helperText={
+                  '選択した新しい店舗に合わせて施設の振込口座を変更します。 ' + (errors.pharmacyId?.message || '')
+                }
+                optionLabel={(option: any) => {
+                  return option?.pharmacyGroup?.name + ' ' + option?.name ?? ''
+                }}
+              />
+              <ControlItemAutocomplete
+                label='請求種別'
+                name='billingType'
+                options={BILLING_TYPE_LIST}
+                control={control}
+                error={!!errors.billingType}
+                helperText={errors.billingType?.message}
+              />
+              {showPaymentType && (
+                <ControlItemAutocomplete
+                  label='支払い種別'
+                  name='paymentType'
+                  options={HEALTH_FACILITY_PAYMENT_TYPE_LIST}
+                  control={control}
+                  error={!!errors.paymentType}
+                  helperText={errors.paymentType?.message}
+                />
+              )}
+              {showAccountManageId && (
+                <ControlAutocomplete
+                  resource='accountManages'
+                  label='振込口座'
+                  name='accountManageId'
+                  defaultId={postsData?.accountManageId ? postsData?.accountManageId : undefined}
+                  control={control}
+                  error={!!errors.accountManageId}
+                  helperText={errors.accountManageId?.message}
+                />
+              )}
+              <ControlItemAutocomplete
+                required
+                label='患者ソート種別'
+                name='patientSortType'
+                options={PATIENT_SORT_TYPE_LIST}
+                control={control}
+                error={!!errors.patientSortType}
+                helperText={
+                  '施設へ一括請求する時の請求書の患者並び順を指定します ' + (errors.patientSortType?.message || '')
+                }
+              />
+            </>
           )}
-          {showAccountManageId && (
-            <ControlAutocomplete
-              resource='accountManages'
-              label='振込口座'
-              name='accountManageId'
-              defaultId={postsData?.accountManageId ? postsData?.accountManageId : undefined}
-              control={control}
-              error={!!errors.accountManageId}
-              helperText={errors.accountManageId?.message}
-            />
-          )}
-          <ControlItemAutocomplete
+          <TextField
             required
-            label='患者ソート種別'
-            name='patientSortType'
-            options={PATIENT_SORT_TYPE_LIST}
-            control={control}
-            error={!!errors.patientSortType}
-            helperText={
-              '施設へ一括請求する時の請求書の患者並び順を指定します ' + (errors.patientSortType?.message || '')
-            }
+            {...register('note')}
+            label='備考'
+            placeholder=''
+            error={!!errors.note}
+            helperText={errors.note?.message}
           />
         </>
       )}
-      <TextField
-        required
-        {...register('note')}
-        label='備考'
-        placeholder=''
-        error={!!errors.note}
-        helperText={errors.note?.message}
-      />
     </Box>
   )
 }
